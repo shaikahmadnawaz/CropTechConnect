51ea612c1e9f16432b274136f77c7272b3234c6d
Shaik Ahmad Nawaz <96189881+shaikahmadnawaz@users.noreply.github.com>
2024-03-25 12:51:03 +0000

    initial commit

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,82 @@
+import React from "react";
+import "./footer.css";
+import { useNavigate } from "react-router-dom";
+import logo from "../img/logo.png";
+import Vector from "../img/Vector.png";
+import Vector1 from "../img/Vector1.png";
+import Vector2 from "../img/Vector2.png";
+import footerBg from "../img/footerBg.png";
+
+const Footer = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="bg-[#219653] inPhone py-20">
+      <div className="flex justify-center items-center">
+        <div className="flex-1 border-r-2 border-black-600">
+          <div
+            className="flex justify-center items-center mx-8 cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            {/* <img src={logo} className="footerLogo" alt="" /> */}
+            <div className="ml-4">
+              <h3 className="text-2xl text-white font-bold mt-4">CropTech Connect</h3>
+              <p className="text-md font-normal text-white mt-2">
+                WeAreFarmerVoice
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 px-16 border-r-2 border-black-600">
+          <div className="flex">
+            <ul className="list-none mr-24">
+              <li
+                className="text-lg text-white font-bold cursor-pointer"
+                onClick={() => navigate("/")}
+              >
+                Home
+              </li>
+              {/* <li className='text-lg text-white font-medium cursor-pointer'>Menu</li> */}
+              <li
+                className="text-lg text-white font-medium cursor-pointer"
+                onClick={() => navigate("/sms")}
+              >
+                SMS Service
+              </li>
+            </ul>
+            <ul>
+              <li
+                className="text-lg text-white font-bold cursor-pointer"
+                onClick={() => navigate("/fertilizer")}
+              >
+                Fertilizer Prediction
+              </li>
+              <li
+                className="text-lg text-white font-medium cursor-pointer"
+                onClick={() => navigate("/crop")}
+              >
+                Crop Prediction
+              </li>
+              <li
+                className="text-lg text-white font-medium cursor-pointer"
+                onClick={() => navigate("/voice")}
+              >
+                Weather Prediction
+              </li>
+              <li
+                className="text-lg text-white font-medium cursor-pointer"
+                onClick={() => navigate("/disease")}
+              >
+                Disease Prediction
+              </li>
+            </ul>
+          </div>
+        
+        </div>
+       
+       
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
